fd8b8ad5ded7502dcd0f84a46ec90a104c5680fa
chrissgchriss <chrissgchriss@gmail.com>
2015-06-12 14:41:16 -0700

    initial commit

---
+++ includes-email.php
@@ -0,0 +1,24 @@
+<form>
+    
+    <div class="form-group">
+    <!--<label for="exampleInputEmail1">Name</label>-->
+    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Name">
+    </div>
+                            
+    <div class="form-group">
+    <!--<label for="exampleInputEmail1">Email address</label>-->
+    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+    </div>
+    
+    <div class="form-group">
+    <!--<label for="exampleInputEmail1">Phone</label>-->
+    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Phone">
+    </div>
+                            
+    <div class="form-group">
+    <textarea class="form-control custom-control" rows="5" placeholder="How may we help?"></textarea>     
+    </div>
+    
+    <button type="submit" class="btn btn-default">Submit</button>
+    
+</form>
---
+++ index2.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Roger Law</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+<?php include('includes-navigation.php'); ?>
+
+    <!-- Header Carousel -->
+    <header>
+        <div></div>
+        
+        <section style="border: thin solid orange; padding-top: 20px; padding-bottom: 20px ">
+            <div class="row row-centered">
+                <div class="col-md-6 col-centered">
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="//www.youtube.com/embed/ePbKGoIGAXY"></iframe>
+                    </div>
+                </div>
+                
+                <div class="col-md-6 col-centered">
+                    <!--<h2 style="margin-bottom: 30px;">Get Help Now</h2>-->
+                    <form>
+                            <div class="form-group">
+                              <!--<label for="exampleInputEmail1">Name</label>-->
+                              <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Name">
+                            </div>
+                            
+                            <div class="form-group">
+                              <!--<label for="exampleInputEmail1">Email address</label>-->
+                              <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                            </div>
+                            
+                            <div class="form-group">
+                              <!--<label for="exampleInputEmail1">Phone</label>-->
+                              <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Phone">
+                            </div>
+                            
+                            <div class="form-group">
+                              <textarea class="form-control custom-control" rows="5" placeholder="How may we help?"></textarea>     
+                              </div>
+                            
+                            <button type="submit" class="btn btn-default">Submit</button>
+                    </form>
+                </div>
+            </div><!-- END ROW -->
+            
+            <div class="row text-center">
+                            <article class="col-md-12">
+                                <h3 class="btn btn-primary">Auto Accidents</h3>
+                                <h3 class="btn btn-primary">Auto Accidents</h3>
+                                <h3 class="btn btn-primary" >Auto Accidents</h3>
+                                <h3 class="btn btn-primary">Auto Accidents</h3>
+                                <h3 class="btn btn-primary">Auto Accidents</h3>
+                            </article>
+                            
+                            <article class="col-md-12">
+                                <h3 class="btn btn-primary">Auto Accident2</h3>
+                                <h3 class="btn btn-primary">Auto Accident2</h3>
+                                <h3 class="btn btn-primary">Auto Accident2</h3>
+                                <h3 class="btn btn-primary">Auto Accident2</h3>
+                                <h3 class="btn btn-primary">Auto Accident2</h3>
+                            </article>
+            </div><!-- END ROW -->
+        </section> 
+    </header>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Marketing Icons Section -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    Welcome to Modern Business
+                </h1>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-check"></i> Bootstrap v3.2.0</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+                        <a href="#" class="btn btn-default">Learn More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-gift"></i> Free &amp; Open Source</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+                        <a href="#" class="btn btn-default">Learn More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-compass"></i> Easy to Use</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+                        <a href="#" class="btn btn-default">Learn More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Portfolio Section -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Portfolio Heading</h2>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <a href="portfolio-item.html">
+                    <img class="img-responsive img-portfolio img-hover" src="" alt="">
+                </a>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <a href="portfolio-item.html">
+                    <img class="img-responsive img-portfolio img-hover" src="" alt="">
+                </a>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <a href="portfolio-item.html">
+                    <img class="img-responsive img-portfolio img-hover" src="" alt="">
+                </a>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <a href="portfolio-item.html">
+                    <img class="img-responsive img-portfolio img-hover" src="" alt="">
+                </a>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <a href="portfolio-item.html">
+                    <img class="img-responsive img-portfolio img-hover" src="" alt="">
+                </a>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <a href="portfolio-item.html">
+                    <img class="img-responsive img-portfolio img-hover" src="" alt="">
+                </a>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Features Section -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Modern Business Features</h2>
+            </div>
+            <div class="col-md-6">
+                <p>The Modern Business template by Start Bootstrap includes:</p>
+                <ul>
+                    <li><strong>Bootstrap v3.2.0</strong>
+                    </li>
+                    <li>jQuery v1.11.0</li>
+                    <li>Font Awesome v4.1.0</li>
+                    <li>Working PHP contact form with validation</li>
+                    <li>Unstyled page elements for easy customization</li>
+                    <li>17 HTML pages</li>
+                </ul>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, omnis doloremque non cum id reprehenderit, quisquam totam aspernatur tempora minima unde aliquid ea culpa sunt. Reiciendis quia dolorum ducimus unde.</p>
+            </div>
+            <div class="col-md-6">
+                <img class="img-responsive" src="" alt="">
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Call to Action Section -->
+        <div class="well">
+            <div class="row">
+                <div class="col-md-8">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, expedita, saepe, vero rerum deleniti beatae veniam harum neque nemo praesentium cum alias asperiores commodi.</p>
+                </div>
+                <div class="col-md-4">
+                    <a class="btn btn-lg btn-default btn-block" href="#">Call to Action</a>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2014</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
+
+</body>
+
+</html>
